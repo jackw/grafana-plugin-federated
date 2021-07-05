fb49b5165ad0aff0e27e7b31a4aa250f3a25d3df
Jack Westbrook <jack.westbrook@gmail.com>
2021-07-05 19:34:47 +0200

    feat: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const _ = require('lodash');
+
+const pluginPackageDeets = require('./package.json');
+const deps = pluginPackageDeets.dependencies;
+const pluginName = pluginPackageDeets.name;
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  context: path.join(__dirname, 'src'),
+  entry: {
+    module: './module.ts',
+  },
+  devtool: 'source-map',
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: 'amd',
+    publicPath: `/public/plugins/${pluginName}/`,
+  },
+  externals: [
+    'lodash',
+    'jquery',
+    'moment',
+    'slate',
+    'emotion',
+    '@emotion/react',
+    '@emotion/css',
+    'prismjs',
+    'slate-plain-serializer',
+    '@grafana/slate-react',
+    'react-redux',
+    // 'react',
+    // 'react-dom',
+    'redux',
+    'rxjs',
+    'react-router-dom',
+    'd3',
+    'angular',
+    '@grafana/ui',
+    '@grafana/runtime',
+    '@grafana/data',
+    function(context, request, callback) {
+      var prefix = 'grafana/';
+      if (request.indexOf(prefix) === 0) {
+        return callback(null, request.substr(prefix.length));
+      }
+      callback();
+    },
+  ],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ModuleFederationPlugin({
+      name: _.snakeCase(pluginName),
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {
+        "./plugin": "./module.ts"
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: deps.react },
+        'react-dom': { singleton: true, requiredVersion: deps['react-dom'] },
+      },
+    }),
+    new HtmlWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'plugin.json', to: '.' },
+        { from: '../README.md', to: '.' },
+        { from: '../CHANGELOG.md', to: '.' },
+        { from: '../LICENSE', to: '.' },
+        { from: 'img/*', to: '.' },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    fallback: {
+      fs: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    moduleIds: 'named',
+    runtimeChunk: true,
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false,
+  },
+};
